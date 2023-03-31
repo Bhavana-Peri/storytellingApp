a5be23f2d59ffd00c6ae4a3e010513f3b9e6e0c4
Bhavana-Peri <88903406+Bhavana-Peri@users.noreply.github.com>
2023-03-31 18:12:31 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCRC7eftpJJ0j7c5_uTrv-6fk0RY8nIAug",
+  authDomain: "storytelling-3d76b.firebaseapp.com",
+  projectId: "storytelling-3d76b",
+  storageBucket: "storytelling-3d76b.appspot.com",
+  messagingSenderId: "886759028902",
+  appId: "1:886759028902:web:48724b3d74823c3fd5dc67"
+};
+
